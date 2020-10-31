2c202cd1e9707cd64682b55d84c2bc63022d206d
Muriel Gonzalez <murielgonzalez@gmail.com>
2020-10-30 21:11:52 -0500

    Login and Welcome screens layout and initial styles

---
+++ PlantTracker/app/src/main/java/com/gonzoapps/planttracker/App.kt
@@ -0,0 +1,12 @@
+package com.gonzoapps.planttracker
+
+import android.app.Application
+import timber.log.Timber
+
+class App : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        Timber.plant(Timber.DebugTree())
+    }
+}
