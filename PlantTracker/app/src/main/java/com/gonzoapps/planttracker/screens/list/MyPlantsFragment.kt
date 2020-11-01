c9e8511913b032746def4fe3e9d797f119ed243c
Muriel Gonzalez <murielgonzalez@gmail.com>
2020-11-01 09:40:41 -0600

    new bg images

--- PlantTracker/app/src/main/java/com/gonzoapps/planttracker/screens/list/MyPlantsFragment.kt
+++ PlantTracker/app/src/main/java/com/gonzoapps/planttracker/screens/list/MyPlantsFragment.kt
@@ -8,14 +8,14 @@ import android.view.ViewGroup
 import com.gonzoapps.planttracker.R
 
 
-class PlantListFragment : Fragment() {
+class MyPlantsFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_plant_list, container, false)
+        return inflater.inflate(R.layout.fragment_my_plants, container, false)
     }
 
 
